ab85aab3bab04381ff5ddce090d8a8ed33aa21e2
Rohan Matta <rohan.matta@cmtelematics.com>
2024-08-01 13:12:53 -0400

    second test

---
+++ model/countryName.java
@@ -0,0 +1,18 @@
+package model;
+
+public class countryName {
+
+    private String countryName;
+
+    public countryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
--- model/dummy.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class dummy {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
